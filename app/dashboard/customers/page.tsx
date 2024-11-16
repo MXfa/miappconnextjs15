e561b48c11d001b058f39578e5de209e9194d005
MXfa <ccramirez85@outlook.com>
2024-11-16 15:48:20 -0600

    se dejo de usaer await en la busqueda

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,29 +1,25 @@
-import CustomersTable from '@/components/shared/customers/table'
-import { fetchFilteredCustomers } from '@/lib/actions/customer.actions'
-import { Metadata } from 'next'
+import CustomersTable from '@/components/shared/customers/table';
+import { fetchFilteredCustomers } from '@/lib/actions/customer.actions';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Customers'
-}
+  title: 'Customers',
+};
 
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
-    query?: string
-   
-  }
+    query?: string;
+  };
 }) {
-  
-// Asegúrate de esperar a searchParams
-
-const { query = '' } = await searchParams || {};
-const customers = await fetchFilteredCustomers(query);
-
+  // No es necesario usar await aquí
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
 
   return (
     <main>
       <CustomersTable customers={customers} />
     </main>
-  )
+  );
 }
